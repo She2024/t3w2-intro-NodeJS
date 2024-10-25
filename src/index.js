6a63c2c84ee22a26436bdc713a327102eb6eb024
she2024 <bullybluedog@hotmail.com>
2024-10-25 22:07:04 +1000

    Feat: Encapsulated try catch in a function block

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const inquirer = require("inquirer").default;
+// const inquirer = require("inquirer").default;
+const promptSync = require('prompt-Sync')();
 
 console.log("Dice Roller Application starts...")
 
@@ -8,6 +9,9 @@ console.log("Dice Roller Application starts...")
  * @returns {number} The side of the dice rolled
  */
 function rollDice(diceSide=6) {
+    if (diceSide <= 0) {
+        throw new error("Dice sides must be greater than 0.");
+    }
     let diceResult = 0;
     diceResult = Math.ceil(Math.random() * diceSide);
     return diceResult;
@@ -15,14 +19,33 @@ function rollDice(diceSide=6) {
 
 // console.log("The side you rolled is: " + rollDice());
 
-const prompt = inquirer.createPromptModule();
+// const prompt = inquirer.createPromptModule();
 
-prompt([
-    {
-        type: "number",
-        name: "dice_side",
-        message: "How many sided dice do you want to roll?"
+// used with inquire not used for promptSync
+// prompt([
+//     {
+//         type: "number",
+//         name: "dice_side",
+//         message: "How many sided dice do you want to roll?"
+//     }
+// ]).then((answer) => {
+//     console.log(rollDice(answer))
+// });
+
+function getDiceSide() {
+const diceSide = promptSync("How many sided dice do you want to roll? ");
+
+try {
+    const parsedDiceSide = parseInt(diceSide);
+    if (isNaN(parsedDiceSide)) {
+        throw new Error('Please enter a valid number.');
     }
-]).then((answer) => {
-    console.log(rollDice(answer))
-});
+    const diceResult = rollDice(parseDiceSide);
+    console.log(`you rolled a ${diceResult}-sided dice.`);
+}
+catch {
+    console.error("You got an error!");
+}
+}
+
+getDiceSide();
